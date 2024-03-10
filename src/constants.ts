8ee88675ec2e71b0c6272b52fdd1a7ab5ee0bcaf
rerelurelu <awaawapurin@gmail.com>
2024-03-10 18:16:59 +0900

    ✨ feat: Add types

--- src/constants.ts
+++ src/constants.ts
@@ -1,8 +1,55 @@
-const PER_PAGE = 9
-const VIEWPORT_OFFSET = '32px'
-const TOAST_WIDTH = 360
-const GAP = 14
-const TOAST_LIFETIME = 5000 // 5s default;
-const TIME_BEFORE_UNMOUNT = 200 // animation duration;
+import type { DocumentMeta } from '@builder.io/qwik-city'
 
-export { GAP, PER_PAGE, TIME_BEFORE_UNMOUNT, TOAST_LIFETIME, TOAST_WIDTH, VIEWPORT_OFFSET }
+export const PER_PAGE = 9
+
+export const VIEWPORT_OFFSET = '32px'
+
+export const TOAST_WIDTH = 360
+
+export const GAP = 14
+
+export const TOAST_LIFETIME = 5000 // 5s default;
+
+export const TIME_BEFORE_UNMOUNT = 200 // animation duration;
+
+export const ENTRIES = [
+	{ href: '/', content: 'home' },
+	{ href: '/blogs/1', content: 'blog' },
+	{ href: '/about', content: 'about' },
+	{ href: '/contact', content: 'contact' },
+]
+
+export const OG_IMAGE: Opengraph = {
+	IMAGE:
+		'https://github.com/rerelurelu/kilonova/assets/43092452/f9f07b5d-2bae-4c17-a95e-14a88dd93579',
+	IMAGE_TYPE: 'image/png',
+	WIDTH: '700',
+	HEIGHT: '700',
+}
+
+export const baseMeta: DocumentMeta[] = [
+	{
+		name: 'type',
+		content: 'website',
+	},
+	{
+		property: 'og:type',
+		content: 'website',
+	},
+	{
+		property: 'og:image',
+		content: OG_IMAGE.IMAGE,
+	},
+	{
+		property: 'og:image:type',
+		content: OG_IMAGE.IMAGE_TYPE,
+	},
+	{
+		property: 'og:image:width',
+		content: OG_IMAGE.WIDTH,
+	},
+	{
+		property: 'og:image:height',
+		content: OG_IMAGE.HEIGHT,
+	},
+]
