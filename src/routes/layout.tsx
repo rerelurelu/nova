e0a50a47d28c1d54705bc897e75c3e5b69f89719
rerelurelu <awaawapurin@gmail.com>
2024-03-10 23:06:05 +0900

    ✨ feat: Add contact form

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -1,22 +1,7 @@
 import { Slot, component$ } from '@builder.io/qwik'
-import { routeLoader$ } from '@builder.io/qwik-city'
-import type { RequestHandler } from '@builder.io/qwik-city'
 import Footer from '~/components/footer/footer'
 import Header from '~/components/header/header'
 import { css } from '~/styled-system/css'
-
-export const onGet: RequestHandler = async ({ cacheControl }) => {
-	cacheControl({
-		staleWhileRevalidate: 60 * 60 * 24 * 7,
-		maxAge: 5,
-	})
-}
-
-export const useServerTimeLoader = routeLoader$(() => {
-	return {
-		date: new Date().toISOString(),
-	}
-})
 
 export default component$(() => {
 	return (
