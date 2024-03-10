1b221f7453a7e362e65acbed51f3406945a38d2c
rerelurelu <awaawapurin@gmail.com>
2024-03-10 18:21:02 +0900

    ✨ feat: Add router-head

--- src/root.tsx
+++ src/root.tsx
@@ -1,6 +1,7 @@
 import { component$ } from '@builder.io/qwik'
 import { QwikCityProvider, RouterOutlet, ServiceWorkerRegister } from '@builder.io/qwik-city'
 
+import { RouterHead } from './components/router-head/router-head'
 import './global.css'
 
 export default component$(() => {
@@ -16,6 +17,7 @@ export default component$(() => {
 			<head>
 				<meta charSet='utf-8' />
 				<link rel='manifest' href='/manifest.json' />
+				<RouterHead />
 				<ServiceWorkerRegister />
 			</head>
 			<body lang='ja'>
