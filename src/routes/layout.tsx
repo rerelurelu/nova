7638b26dd0a70c441d9f73b60023119f9232e430
rerelurelu <awaawapurin@gmail.com>
2024-03-10 16:29:53 +0900

    🎉 init: Initial commit

---
+++ src/routes/layout.tsx
@@ -0,0 +1,26 @@
+import { Slot, component$ } from '@builder.io/qwik'
+import { routeLoader$ } from '@builder.io/qwik-city'
+import type { RequestHandler } from '@builder.io/qwik-city'
+
+export const onGet: RequestHandler = async ({ cacheControl }) => {
+	cacheControl({
+		staleWhileRevalidate: 60 * 60 * 24 * 7,
+		maxAge: 5,
+	})
+}
+
+export const useServerTimeLoader = routeLoader$(() => {
+	return {
+		date: new Date().toISOString(),
+	}
+})
+
+export default component$(() => {
+	return (
+		<>
+			<main>
+				<Slot />
+			</main>
+		</>
+	)
+})
