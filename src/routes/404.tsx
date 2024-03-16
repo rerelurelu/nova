6af307c33b0255043bd161371edf584af4842310
rerelurelu <awaawapurin@gmail.com>
2024-03-16 19:21:06 +0900

    🥱 style: Use spaces for indentation

--- src/routes/404.tsx
+++ src/routes/404.tsx
@@ -5,65 +5,65 @@ import { css } from '~/styled-system/css'
 import { divider } from '~/styled-system/patterns'
 
 export default component$(() => {
-	return (
-		<div class={wrapper}>
-			<div class={container}>
-				<h1 class={title}>404</h1>
-				<div
-					class={divider({
-						orientation: 'vertical',
-						thickness: '0.125rem',
-						color: 'divider',
-						h: '3.5rem',
-						mx: '1.5rem',
-					})}
-				/>
-				<h2 class={info}>This page could not be found.</h2>
-			</div>
-		</div>
-	)
+  return (
+    <div class={wrapper}>
+      <div class={container}>
+        <h1 class={title}>404</h1>
+        <div
+          class={divider({
+            orientation: 'vertical',
+            thickness: '0.125rem',
+            color: 'divider',
+            h: '3.5rem',
+            mx: '1.5rem',
+          })}
+        />
+        <h2 class={info}>This page could not be found.</h2>
+      </div>
+    </div>
+  )
 })
 
 export const head: DocumentHead = {
-	title: '404 This page could not be found.',
-	meta: [
-		...BASE_META,
-		{
-			name: 'description',
-			content: '404 This page could not be found.',
-		},
-		{
-			property: 'og:title',
-			content: '404 This page could not be found.',
-		},
-		{
-			property: 'og:description',
-			content: '404 This page could not be found.',
-		},
-	],
+  title: '404 This page could not be found.',
+  meta: [
+    ...BASE_META,
+    {
+      name: 'description',
+      content: '404 This page could not be found.',
+    },
+    {
+      property: 'og:title',
+      content: '404 This page could not be found.',
+    },
+    {
+      property: 'og:description',
+      content: '404 This page could not be found.',
+    },
+  ],
 }
 
 const wrapper = css({
-	display: 'grid',
-	placeItems: 'center',
-	h: '70vh',
-	px: '3rem',
+  display: 'grid',
+  placeItems: 'center',
+  h: '70vh',
+  px: '3rem',
 })
 
 const container = css({
-	display: 'flex',
-	alignItems: 'center',
-	justifyContent: 'center',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
 })
 
 const title = css({
-	color: 'white',
-	fontSize: '2.25rem',
-	lineHeight: '2.5rem',
+  color: 'white',
+  fontSize: '2.25rem',
+  lineHeight: '2.5rem',
 })
 
 const info = css({
-	color: 'white',
-	fontSize: '1.125rem',
-	lineHeight: '1.75rem',
+  color: 'white',
+  fontSize: '1.125rem',
+  lineHeight: '1.75rem',
 })
--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -4,25 +4,25 @@ import Header from '~/components/header/header'
 import { css } from '~/styled-system/css'
 
 export default component$(() => {
-	return (
-		<>
-			<Header />
-			<main class={rootLayout}>
-				<div class={container}>
-					<Slot />
-				</div>
-			</main>
-			<Footer />
-		</>
-	)
+  return (
+    <>
+      <Header />
+      <main class={rootLayout}>
+        <div class={container}>
+          <Slot />
+        </div>
+      </main>
+      <Footer />
+    </>
+  )
 })
 
 const rootLayout = css({
-	minH: '100vh',
+  minH: '100vh',
 })
 
 const container = css({
-	px: { base: '1.25rem', md: '3rem' },
-	pt: '4rem',
-	pb: { base: '8rem', lg: '12rem' },
+  px: { base: '1.25rem', md: '3rem' },
+  pt: '4rem',
+  pb: { base: '8rem', lg: '12rem' },
 })
