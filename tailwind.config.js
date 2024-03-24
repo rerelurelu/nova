6cb28957a07472a98230ce4eb84ad62cde42df50
rerelurelu <awaawapurin@gmail.com>
2024-03-24 22:34:53 +0900

    feat: Add tailwind settings

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,22 @@
+import defaultTheme from 'tailwindcss/defaultTheme'
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
-    extend: {},
+    colors: {
+      bg: '#1a1e2e',
+      main: '#bdc6e9',
+      hero: '#fde047',
+      bgHeader: '#1a1e2e4d',
+      lightCyan: '#00f1f9',
+      lightPurple: '#cb33f4',
+    },
+    extend: {
+      fontFamily: {
+        sans: ['Overpass', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [require('daisyui')],
 }
